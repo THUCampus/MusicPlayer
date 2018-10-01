f6b05f2711c72063e1e730079751fa5ee44be036
JunguangJiang <13126830206@163.com>
2018-10-01 20:12:45 +0800

    Initial commit

---
+++ dialog.asm
@@ -0,0 +1,38 @@
+.386
+
+.model	flat, stdcall
+option	casemap :none
+
+include		dialog.inc
+
+.code
+start:
+	invoke	GetModuleHandle, NULL
+	mov	hInstance, eax
+	invoke	InitCommonControls
+	invoke	DialogBoxParam, hInstance, IDD_MAIN, 0, offset DlgProc, 0
+	invoke	ExitProcess, eax
+
+DlgProc proc hWin:DWORD,uMsg:DWORD,wParam:DWORD,lParam:DWORD
+	mov	eax,uMsg
+	
+	.if	eax == WM_INITDIALOG
+		invoke	LoadIcon,hInstance,200
+		invoke	SendMessage, hWin, WM_SETICON, 1, eax
+	.elseif eax == WM_COMMAND
+		mov	eax,wParam
+		.if	eax == IDB_EXIT
+			invoke	SendMessage, hWin, WM_CLOSE, 0, 0
+		.elseif eax == IDC_PlayButton
+			invoke mciSendString, ADDR OPENMP3, NULL, 0, NULL
+			invoke mciSendString, ADDR PLAYMP3, NULL, 0, NULL
+		.endif
+	.elseif	eax == WM_CLOSE
+		invoke	EndDialog, hWin, 0
+	.endif
+
+	xor	eax,eax
+	ret
+DlgProc endp
+
+end start
